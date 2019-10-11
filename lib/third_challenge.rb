109580609ca06a8df3af8d6405cd3a2481048672
Ronda D Cowen <cowenhome5@gmail.com>
2019-10-11 14:00:38 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -17,10 +17,10 @@ def third_challenge
       :patriarch => {:name => "Lord Capulet",
         :age => "50"
                 },
-      :matriarch => {
-        #your key/value pairs here
+      :matriarch => {:name => "Lady Capulet",
+        :age => "51"
         },
-      :heroine => {
+      :heroine => {:name "Juliet"
         #your key/value pairs here
         },
       :heroine_friends => []
